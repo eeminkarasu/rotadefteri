5bede7e3361d73b6c552b1b780c0bcadaad6ace9
eeminkarasu <elaemin16@gmail.com>
2026-09-10 14:45:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD='v8-4-1-real-ui-fix';
+const BUILD='v8-4-2-cloud-sync-safe';
 const SHELL_CACHE='rotadefteri-shell-'+BUILD;
 const RUNTIME_CACHE='rotadefteri-runtime-'+BUILD;
 const SHELL=['./','./index.html','./firebase-config.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/brand-mark.svg','./icons/nav-arrow.svg','./icons/phone.svg','./icons/note.svg'];
